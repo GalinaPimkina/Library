0c6e9b382504abda4afda612ef49aef6f034602e
Galina <novikova-box@yandex.ru>
2025-05-22 16:24:25 +0300

    add id in fake_library_db

--- api/routers/books.py
+++ api/routers/books.py
@@ -2,30 +2,36 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-fake_library_db = {
-    1: {
+fake_library_db = [
+    {
+        "id": 1,
         "author": "Н.В. Гоголь",
         "title": "Мертвые души",
         "amount": 4,
     },
-    2: {
+    {
+        "id": 2,
         "author": "Ф.М. Достоевский",
         "title": "Преступление и наказание",
         "amount": 7,
     },
-    3: {
+    {
+        "id": 3,
         "author": "А.С. Пушкин",
         "title": "Евгений Онегин",
         "amount": 6,
     },
-    4: {
+    {
+        "id": 4,
         "author": "М.Ю. Лермонтов",
         "title": "Герой нашего времени",
         "amount": 5,
     },
-    5: {
+    {
+        "id": 5,
         "author": "И.С. Тургенев",
         "title": "Отцы и дети",
         "amount": 2,
     },
-}
+]
+
