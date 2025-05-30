0c073f640ac925edd59d40329c4404bc76751f41
Galina <novikova-box@yandex.ru>
2025-05-30 14:11:46 +0300

    add requirements.txt

--- api/__init__.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from .routers.books import router as books_router
-
-
-router = APIRouter()
-router.include_router(router=books_router, prefix="/books")
--- api/schemas/books.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class Book(BaseModel):
-    id: int
-    author: str
-    title: str
-    amount: int
