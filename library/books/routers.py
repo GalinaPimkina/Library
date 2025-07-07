6a4ce9d7650e7430268a39a975f18e765ee508f5
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-07-07 15:54:05 +0300

    add response model to get_all_books router

--- library/books/routers.py
+++ library/books/routers.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
+
+from books.schemas import BaseBook
 from library.books.crud import ReadBook
 
 router = APIRouter(prefix='/books', tags=['Книги'])
 
 
-@router.get("/", summary="Получить список всех книг в библиотеке")
+@router.get("/", summary="Получить список всех книг в библиотеке", response_model=list[BaseBook])
 async def get_all_books():
     return await ReadBook.read_all_books()
