db756288019240f69a358e6b7318de7811559ef0
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-07-07 15:36:37 +0300

    include router_books

--- library/main.py
+++ library/main.py
@@ -1,7 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
+from library.books.routers import router as router_books
 
 app = FastAPI()
+
+app.include_router(router_books)
 
 
 if __name__ == "__main__":
