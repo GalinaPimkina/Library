f79fabf605463c85f15a4319880ef13bf4f1a574
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-07-08 15:59:26 +0300

    add router get_book_from_id

--- library/books/routers.py
+++ library/books/routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -13,5 +13,13 @@ router = APIRouter(prefix='/books', tags=['Книги'])
 async def get_all_books(session: AsyncSession = Depends(get_session)):
     books = await session.execute(select(Book).order_by(Book.title))
     return books.scalars().all()
+
+
+@router.get("/{book_id}/", summary="Получить книгу по id", response_model=BookPublic)
+async def get_book_from_id(book_id: int, session: AsyncSession = Depends(get_session)):
+    book = await session.get(Book, book_id)
+    if not book:
+        raise HTTPException(status_code=404, detail="Книга не найдена")
+    return book
 
 
