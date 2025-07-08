206babaa4e0ded469f4cac167fbe615cf9279c3e
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-07-08 14:50:04 +0300

    init get_session dependensy

--- library/database.py
+++ library/database.py
@@ -1,13 +1,24 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
-from sqlalchemy.orm import DeclarativeBase, declared_attr
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession, AsyncAttrs
+from sqlalchemy.orm import declarative_base, DeclarativeBase
 
 from config import get_db_url
 
 
 DATABASE_URL = get_db_url()
 engine = create_async_engine(DATABASE_URL)
-async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
+
+
+async def init_models():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
