53a8243415570aaacde59606d83d971c138f983b
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-07-08 14:51:20 +0300

    rewrite get_all_books router with async session depends

--- library/books/routers.py
+++ library/books/routers.py
@@ -1,11 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from books.schemas import BaseBook
-from library.books.crud import ReadBook
+from books.models import Book
+from books.schemas import BookPublic
+from database import get_session
 
 router = APIRouter(prefix='/books', tags=['Книги'])
 
 
-@router.get("/", summary="Получить список всех книг в библиотеке", response_model=list[BaseBook])
-async def get_all_books():
-    return await ReadBook.read_all_books()
+@router.get("/", summary="Получить список всех книг в библиотеке", response_model=list[BookPublic])
+async def get_all_books(session: AsyncSession = Depends(get_session)):
+    books = await session.execute(select(Book).order_by(Book.title))
+    return books.scalars().all()
+
+
