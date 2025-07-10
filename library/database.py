bf051a7d3fe798d8eeb9546f550edc9ccd956145
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-07-10 14:03:49 +0300

    add fields id, created_at, updated_at to Base class

--- library/database.py
+++ library/database.py
@@ -1,24 +1,34 @@
+from datetime import datetime
+from sqlalchemy import func
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession, AsyncAttrs
-from sqlalchemy.orm import declarative_base, DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
 
 from config import get_db_url
 
 
 DATABASE_URL = get_db_url()
 engine = create_async_engine(DATABASE_URL)
-async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
 
 
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower() + "s"
 
 
 async def init_models():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
-
-
-async def get_session() -> AsyncSession:
-    async with async_session() as session:
-        yield session
