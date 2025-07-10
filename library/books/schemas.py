8d2ba325003677cafbaed6bff30dbf5b447a0b22
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-07-10 17:15:07 +0300

    edit pydantic schemas

--- library/books/schemas.py
+++ library/books/schemas.py
@@ -1,10 +1,28 @@
+from datetime import datetime
+
 from pydantic import BaseModel, ConfigDict
 
+from models import Student
 
-class BookPublic(BaseModel):
+
+class BookSystem(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
+    created_at: datetime
+    updated_at: datetime
+
+
+class BookPublic(BookSystem):
     title: str
+    author: str
+    publish_date: int
     total_amount: int
     taken_amount: int
+
+
+class BookStudent(BookSystem):
+    title: str
+    taken_amount: int
+    # students: list["Student"]
+
