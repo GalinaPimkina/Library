5fbfc2d72b8940f51e2fa8274e2c9b3b8a2fd4c6
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-07-11 14:16:14 +0300

    create router get_book_from_id

--- library/books/routers.py
+++ library/books/routers.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
 from models import Book
-from books.schemas import BookPublic
+from books.schemas import BookPublic, BookListStudent
 from database import get_session
 
 router = APIRouter(prefix='/books', tags=['Книги'])
@@ -16,6 +16,15 @@ async def get_all_books(session: AsyncSession = Depends(get_session)):
     result = books.scalars().all()
     if not result:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Книги не найдены")
+    return result
+
+
+@router.get("/{book_id}", summary="Книга по id", response_model=list[BookListStudent], status_code=status.HTTP_200_OK)
+async def get_book_from_id(book_id: int, session: AsyncSession = Depends(get_session)):
+    book = await session.execute(select(Book).where(Book.id == book_id))
+    result = book.scalars().all()
+    if not result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Книга не найдена")
     return result
 
 
