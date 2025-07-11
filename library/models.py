e5ed3325a45370302c7f611fbc7fd3a989c470c0
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-07-11 14:15:35 +0300

    add lazy="selectin" in Book model

--- library/models.py
+++ library/models.py
@@ -15,7 +15,7 @@ class Book(Base):
     total_amount: Mapped[int | None] = mapped_column(default=1, server_default="1") #всего книг в библиотеке
     taken_amount: Mapped[int | None] = mapped_column(default=0, server_default="0") #взятые книги
 
-    students: Mapped[list["Student"]] = relationship(secondary="students_books", back_populates="books")
+    students: Mapped[list["Student"]] = relationship(secondary="students_books", back_populates="books", lazy="selectin")
 
     def __str__(self):
         return str(self.title)
