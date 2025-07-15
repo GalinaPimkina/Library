ed2d6727b3e05785c434415f763604d68b653264
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-07-15 14:11:14 +0300

    add new router to main.py app

--- library/main.py
+++ library/main.py
@@ -1,10 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 from library.books.routers import router as router_books
+from library.students.routers import router as router_students
 
 app = FastAPI()
 
 app.include_router(router_books)
+app.include_router(router_students)
 
 
 if __name__ == "__main__":
