9d15667b3d267d9cb8b4ccdd87c6c1822c50c712
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-07-18 15:22:27 +0300

    test_get_book_by_title v1

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,6 +4,45 @@ from httpx import AsyncClient, ASGITransport
 from library.main import app
 
 
+@pytest.mark.parametrize(
+    "query, status_code, res",
+    [
+        (
+                "онегин",
+                200,
+                [{
+                    "title": "онегин",
+                    "author": "пушкин",
+                    "publish_date": 2006,
+                    "total_amount": 10
+                }]
+        ),
+        (
+                "о",
+                200,
+                [
+                    {
+                        "title": "онегин",
+                        "author": "пушкин",
+                        "publish_date": 2006,
+                        "total_amount": 10
+                    },
+                    {
+                        "title": "пикник на обочине",
+                        "author": "братья стругацкие",
+                        "publish_date": 2022,
+                        "total_amount": 7
+                    }
+                ]
+        ),
+        (
+                "екореро",
+                404,
+                {
+                    "detail": "Книга не найдена"
+                }),
+    ]
+)
 @pytest.mark.asyncio
 async def test_get_book_by_title():
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
