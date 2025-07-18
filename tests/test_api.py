53e78a4fa9fd2e9b9f37fc14368667d43968f305
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-07-18 15:22:43 +0300

    test_get_book_by_id v1

--- tests/test_api.py
+++ tests/test_api.py
@@ -44,14 +44,61 @@ from library.main import app
     ]
 )
 @pytest.mark.asyncio
-async def test_get_book_by_title():
+async def test_get_book_by_title(query, status_code, res):
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
-        response = await client.get("/books/search/?title=онегин")
-        assert response.status_code == 200
-        assert response.json() == [{
-            "title": "онегин",
-            "author": "пушкин",
-            "publish_date": 2006,
-            "total_amount": 10
-        }]
+        response = await client.get(f"/books/search/?title={query}")
+        assert response.status_code == status_code
+        assert response.json() == res
+
+
+@pytest.mark.parametrize(
+    "book_id, status_code, res",
+    [
+        (
+            1,
+            200,
+            {
+                "title": "KOLOBOK",
+                "author": "бабушка и дедушка",
+                "publish_date": 1996,
+                "total_amount": 3,
+                "students": []
+            }
+        ),
+        (
+            100,
+            404,
+            {
+                "detail": "Книга не найдена"
+            }
+        ),
+        (
+            0,
+            422,
+            {
+                "detail":  [
+                    {
+                        "ctx":
+                            {
+                                "ge": 1,
+                            },
+                        "input": "0",
+                        "loc": [
+                            "path",
+                            "book_id",
+                            ],
+                        "msg": "Input should be greater than or equal to 1",
+                        "type": "greater_than_equal",
+                    },
+                        ]
+            }
+        ),
+    ]
+)
+@pytest.mark.asyncio
+async def test_get_book_by_id(book_id, status_code, res):
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
+        response = await client.get(f"/books/{book_id}/")
+        assert response.status_code == status_code
+        assert response.json() == res
 
