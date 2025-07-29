d1912032653749d7a22d6ca00d9329b7cb1e1e2c
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-07-29 15:56:05 +0300

    отладка тестов

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,6 @@
 import pytest
 import pytest_asyncio
+
 from fastapi import HTTPException
 from httpx import AsyncClient, ASGITransport
 
@@ -19,7 +20,7 @@ class TestBook:
         "query, status_code, res, expected_exception",
         [
             ("онегин",200,[{"title": "онегин","author": "пушкин","publish_date": 2006,"total_amount": 10, "id": 2},],None,),
-            # ("о",200,[{"title": "онегин","author": "пушкин","publish_date": 2006,"total_amount": 10},{"title": "пикник на обочине","author": "братья стругацкие","publish_date": 2022,"total_amount": 7}],None,),
+            ("о",200,[{"title": "онегин","author": "пушкин","publish_date": 2006,"total_amount": 10, "id": 2},{"title": "пикник на обочине","author": "братья стругацкие","publish_date": 2022,"total_amount": 7, "id": 3}],None,),
             ("екореро",404,{"detail": "Книга не найдена"},HTTPException,),
         ]
     )
@@ -33,7 +34,7 @@ class TestBook:
     @pytest.mark.parametrize(
         "book_id, status_code, res, expected_exception",
         [
-            (1, 200, {"title": "KOLOBOK", "author": "бабушка и дедушка", "publish_date": 1996, "total_amount": 3, "students": []}, None,),
+            (1, 200, {"title": "KOLOBOK", "author": "бабушка и дедушка", "publish_date": 1996, "total_amount": 3, 'students':[{'full_name': 'семенов семен семенович','group_number': '34вп',},]}, None,),
             (100, 404, {"detail": "Книга не найдена"}, HTTPException,),
             (0, 422, {"detail": [{"ctx": {"ge": 1,}, "input": "0", "loc": ["path", "book_id",], "msg": "Input should be greater than or equal to 1", "type": "greater_than_equal",},]}, HTTPException,),
         ]
