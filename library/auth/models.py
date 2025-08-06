2ea50a27b07baa527267c7f4d80695ee1f8cbd2e
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-08-06 16:32:02 +0300

    put AccessToken model to auth/models.py

--- library/auth/models.py
+++ library/auth/models.py
@@ -1,10 +1,12 @@
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Mapped
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyBaseAccessTokenTable, SQLAlchemyAccessTokenDatabase
 
-from library.database import Base
-from library.database import get_session
+
+from library.database import Base, get_session
 
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
@@ -13,3 +15,14 @@ class User(Base, SQLAlchemyBaseUserTable[int]):
     @classmethod
     def get_db(cls, session: AsyncSession = Depends(get_session)):
         return SQLAlchemyUserDatabase(session, User)
+
+
+class AccessToken(Base, SQLAlchemyBaseAccessTokenTable[int]):
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="cascade"),
+        nullable=False,
+    )
+
+    @classmethod
+    def get_db(cls, session: AsyncSession = Depends(get_session)):
+        return SQLAlchemyAccessTokenDatabase(session, cls)
