6a42abfe05d30b27bc78767ebe1a0191b84aef30
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-08-11 12:50:30 +0300

    add get_user_db

--- library/models/users.py
+++ library/models/users.py
@@ -1,8 +1,15 @@
+from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTable
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped
 
-from library.database.db import Base
+from library.database.db import Base, get_session
 
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
     username: Mapped[str]
+
+
+async def get_user_db(session: AsyncSession = Depends(get_session)):
+    yield SQLAlchemyUserDatabase(session, User)
