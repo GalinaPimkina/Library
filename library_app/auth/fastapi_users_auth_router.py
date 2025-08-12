7a3c92fb28c8e689a9bc274727c074c00048648b
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-08-12 16:06:50 +0300

    create fastapi_users

---
+++ library_app/auth/fastapi_users_auth_router.py
@@ -0,0 +1,11 @@
+from fastapi_users import FastAPIUsers
+
+from .backend import authentication_backend
+from .user_manager import get_user_manager
+from models.users import User
+
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [authentication_backend],
+)
