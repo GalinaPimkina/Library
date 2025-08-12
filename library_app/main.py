3d103a9dbd662035fab9d282767eb0df2d59d55b
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-08-12 16:07:51 +0300

    add auth router

--- library_app/main.py
+++ library_app/main.py
@@ -2,11 +2,13 @@ import uvicorn
 from fastapi import FastAPI
 from routers.books import router as router_books
 from routers.students import router as router_students
+from routers.auth import router as auth_router
 
 app = FastAPI()
 
 app.include_router(router_books)
 app.include_router(router_students)
+app.include_router(auth_router)
 
 
 if __name__ == "__main__":
