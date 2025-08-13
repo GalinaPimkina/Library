c3527fe79a19f932d24e6f86eaff52876ac3614a
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-08-13 14:24:50 +0300

    add dependency bearer

--- library_app/routers/auth/__init__.py
+++ library_app/routers/auth/__init__.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from .authentication import router as auth_router
 from .register import router as register_router
@@ -6,7 +6,12 @@ from .verify import router as verify_router
 from .reset_password import router as reset_password_router
 from .manage_user import router as manage_user_router
 
-router = APIRouter(tags=["Auth"])
+from fastapi.security import HTTPBearer
+
+
+http_bearer = HTTPBearer(auto_error=False)
+
+router = APIRouter(tags=["Auth"], dependencies=[Depends(http_bearer)])
 router.include_router(auth_router)
 router.include_router(register_router)
 router.include_router(verify_router)
