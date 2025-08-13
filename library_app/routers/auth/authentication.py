7da0cb917aef423dbd347850050500df040d9c34
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-08-13 13:47:22 +0300

    create authentication router

--- library_app/routers/auth/authentication.py
+++ library_app/routers/auth/authentication.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from auth.backend import authentication_backend
 from auth.fastapi_users_auth_router import fastapi_users
 
-router = APIRouter(prefix="/auth", tags=["Auth"])
+router = APIRouter(prefix="/authentication")
 
 router.include_router(fastapi_users.get_auth_router(authentication_backend))
