c8663daf6b75577e4afe27462e0814c8d6957512
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-08-13 13:49:32 +0300

    combine all auth routers in one

---
+++ library_app/routers/auth/__init__.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+from .authentication import router as auth_router
+from .register import router as register_router
+from .verify import router as verify_router
+from .reset_password import router as reset_password_router
+from .manage_user import router as manage_user_router
+
+router = APIRouter(tags=["Auth"])
+router.include_router(auth_router)
+router.include_router(register_router)
+router.include_router(verify_router)
+router.include_router(reset_password_router)
+router.include_router(manage_user_router)
