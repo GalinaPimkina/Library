cc081e6f83ead253b49d3376587f8cc5a9e24c8e
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-08-13 14:24:29 +0300

    fix imports

--- library_app/models/access_token.py
+++ library_app/models/access_token.py
@@ -5,10 +5,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database.db import Base, get_session
-from .users import User
 
 
-class AccessToken(Base, SQLAlchemyBaseAccessTokenTable[int]):
+class AccessToken(SQLAlchemyBaseAccessTokenTable[int], Base):
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="cascade"), nullable=False)
 
 
