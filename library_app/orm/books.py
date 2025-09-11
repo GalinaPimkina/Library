a9719368346d2a8f20697c3ef10c124ccb8bc3d0
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-09-11 16:16:25 +0300

    create staticmethod get_book_from_title

--- library_app/orm/books.py
+++ library_app/orm/books.py
@@ -1,2 +1,21 @@
+from fastapi import HTTPException
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+
+from models.books import Book
+
+
 class BookAsyncORM:
-    pass
+    @staticmethod
+    async def get_book_from_title(title: str, async_session: AsyncSession):
+        async with async_session as session:
+            query = (
+                select(Book)
+                .filter(Book.title.ilike(f"%{title}%"))
+            )
+            res = await session.execute(query)
+            result = res.scalars().all()
+            if not result:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Книга не найдена")
+            return result
