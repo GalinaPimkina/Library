15ac9a8bf1ee3fc39a815845e724755eb907f0b1
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-09-15 13:50:02 +0300

    create staticmethod get_all_students

--- library_app/orm/students.py
+++ library_app/orm/students.py
@@ -1,2 +1,17 @@
-class AsyncORM:
-    pass
+from fastapi import HTTPException
+from sqlalchemy import select
+from starlette import status
+
+from models.students import Student
+
+
+class StudentAsyncORM:
+    @staticmethod
+    async def get_all_students(async_session):
+        async with async_session as session:
+            query = (select(Student).order_by(Student.id))
+            result = await session.execute(query)
+            students = result.scalars().all()
+            if not students:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Студенты не найдены")
+            return students
